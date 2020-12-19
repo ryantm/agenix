de625b5298cbe12d970bcdd29c263103b239ad1e
Ryan Mulligan <ryan@ryantm.com>
2020-12-18 19:57:48 -0800

    add friendlier error message in the event of no identity
    fixes #6

--- pkgs/agenix.nix
+++ pkgs/agenix.nix
@@ -111,6 +111,10 @@ function edit {
         if [ -f "$HOME/.ssh/id_ed25519" ]; then
             DECRYPT+=(--identity "$HOME/.ssh/id_ed25519")
         fi
+        if [[ "''${DECRYPT[*]}" != *"--identity"* ]]; then
+          echo "No identity found to decrypt $FILE. Try adding an SSH key at $HOME/.ssh/id_rsa or $HOME/.ssh/id_ed25519 or using the --identity flag to specify a file."
+          exit 1
+        fi
         DECRYPT+=(-o "$CLEARTEXT_FILE" "$FILE")
         ${ageBin} "''${DECRYPT[@]}" || exit 1
         cp "$CLEARTEXT_FILE" "$CLEARTEXT_FILE.before"
