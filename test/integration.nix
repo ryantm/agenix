6cb257097677c3f78fa60bca94bd566c1243b605
Vincent Haupert <mail@vincent-haupert.de>
2021-09-14 17:05:23 +0200

    tests/integration: add dry-activate test

--- test/integration.nix
+++ test/integration.nix
@@ -39,20 +39,31 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ pkgs, ...}: {
     password = "password1234";
   in ''
     system1.wait_for_unit("multi-user.target")
-    system1.wait_until_succeeds("pgrep -f 'agetty.*tty1'")
-    system1.sleep(2)
-    system1.send_key("alt-f2")
-    system1.wait_until_succeeds("[ $(fgconsole) = 2 ]")
-    system1.wait_for_unit("getty@tty2.service")
-    system1.wait_until_succeeds("pgrep -f 'agetty.*tty2'")
-    system1.wait_until_tty_matches(2, "login: ")
-    system1.send_chars("${user}\n")
-    system1.wait_until_tty_matches(2, "login: ${user}")
-    system1.wait_until_succeeds("pgrep login")
-    system1.sleep(2)
-    system1.send_chars("${password}\n")
-    system1.send_chars("whoami > /tmp/1\n")
-    system1.wait_for_file("/tmp/1")
-    assert "${user}" in system1.succeed("cat /tmp/1")
+
+    with subtest("Decrypting age-encrypted ${user} password works"):
+        system1.succeed("test -e /run/secrets/passwordfile-user1")
+
+    with subtest("Logging in with ${user} works"):
+        system1.wait_until_succeeds("pgrep -f 'agetty.*tty1'")
+        system1.sleep(2)
+        system1.send_key("alt-f2")
+        system1.wait_until_succeeds("[ $(fgconsole) = 2 ]")
+        system1.wait_for_unit("getty@tty2.service")
+        system1.wait_until_succeeds("pgrep -f 'agetty.*tty2'")
+        system1.wait_until_tty_matches(2, "login: ")
+        system1.send_chars("${user}\n")
+        system1.wait_until_tty_matches(2, "login: ${user}")
+        system1.wait_until_succeeds("pgrep login")
+        system1.sleep(2)
+        system1.send_chars("${password}\n")
+        system1.send_chars("whoami > /tmp/1\n")
+        system1.wait_for_file("/tmp/1")
+        assert "${user}" in system1.succeed("cat /tmp/1")
+
+    with subtest("Decrypted password not moved on dry-activation"):
+        system1.succeed("rm /run/secrets/passwordfile-user1")
+        out = system1.succeed("/run/current-system/bin/switch-to-configuration dry-activate")
+        assert "[agenix] dry-run, not moving decrypted secret" in out
+        system1.succeed('test -z "$(ls -A /run/secrets)"')
   '';
 }) args
