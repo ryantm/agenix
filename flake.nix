64b05745148c72a1740a54418d1f91b9b361f323
Ryan Mulligan <ryan@ryantm.com>
2023-01-29 08:02:57 -0800

    feature: update flake output format and docs

--- flake.nix
+++ flake.nix
@@ -1,38 +1,37 @@
 {
   description = "Secret management with age";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-  let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     agenix = system: nixpkgs.legacyPackages.${system}.callPackage ./pkgs/agenix.nix {};
   in {
-
     nixosModules.age = import ./modules/age.nix;
-    nixosModule = self.nixosModules.age;
+    nixosModules.default = self.nixosModules.age;
 
-    overlay = import ./overlay.nix;
+    overlays.default = import ./overlay.nix;
 
-    packages."aarch64-linux".agenix = agenix "aarch64-linux";
-    defaultPackage."aarch64-linux" = self.packages."aarch64-linux".agenix;
+    packages.aarch64-linux.agenix = agenix "aarch64-linux";
+    packages.aarch64-linux.default = self.packages.aarch64-linux.agenix;
 
-    packages."i686-linux".agenix = agenix "i686-linux";
-    defaultPackage."i686-linux" = self.packages."i686-linux".agenix;
+    packages.i686-linux.agenix = agenix "i686-linux";
+    packages.i686-linux.default = self.packages.i686-linux.agenix;
 
-    packages."x86_64-darwin".agenix = agenix "x86_64-darwin";
-    defaultPackage."x86_64-darwin" = self.packages."x86_64-darwin".agenix;
+    packages.x86_64-darwin.agenix = agenix "x86_64-darwin";
+    packages.x86_64-darwin.default = self.packages.x86_64-darwin.agenix;
 
-    packages."aarch64-darwin".agenix = agenix "aarch64-darwin";
-    defaultPackage."aarch64-darwin" = self.packages."aarch64-darwin".agenix;
+    packages.aarch64-darwin.agenix = agenix "aarch64-darwin";
+    packages.aarch64-darwin.default = self.packages.aarch64-darwin.agenix;
 
-    packages."x86_64-linux".agenix = agenix "x86_64-linux";
-    defaultPackage."x86_64-linux" = self.packages."x86_64-linux".agenix;
-    checks."x86_64-linux".integration = import ./test/integration.nix {
-      inherit nixpkgs; pkgs = nixpkgs.legacyPackages."x86_64-linux"; system = "x86_64-linux";
+    packages.x86_64-linux.agenix = agenix "x86_64-linux";
+    packages.x86_64-linux.default = self.packages.x86_64-linux.agenix;
+    checks.x86_64-linux.integration = import ./test/integration.nix {
+      inherit nixpkgs;
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      system = "x86_64-linux";
     };
-
   };
-
 }
