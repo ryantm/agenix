2cf2bf1ffcda3efad09182eaa6011af78b2b33eb
Ryan Mulligan <ryan@ryantm.com>
2023-02-20 12:04:09 -0800

    feature: enable one-way encrypted secrets
    fixes #9

--- test/fixtures/one-way/secrets.nix
+++ test/fixtures/one-way/secrets.nix
@@ -1,5 +1,6 @@
 let
   system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPJDyIr/FSz1cJdcoW69R+NrWzwGK/+3gJpqD1t8L2zE";
-in {
-  "one-way.age".publicKeys = [system1];
+in
+{
+  "one-way.age".publicKeys = [ system1 ];
 }
