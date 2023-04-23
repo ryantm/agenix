1f43d94d52a750270202f22d50598e6234aecf86
Bruno BELANYI <bruno@belanyi.fr>
2023-04-23 11:22:03 +0100

    Add home-manager input

--- flake.nix
+++ flake.nix
@@ -7,12 +7,17 @@
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     darwin,
+    home-manager,
   }: let
     agenix = system: nixpkgs.legacyPackages.${system}.callPackage ./pkgs/agenix.nix {};
     doc = system: nixpkgs.legacyPackages.${system}.callPackage ./pkgs/doc.nix {};
