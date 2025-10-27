246b28aba2dd18dcab02188331dbe7894d388380
Marien Zwart <marien.zwart@gmail.com>
2025-10-28 00:51:06 +1100

    contrib: reduce code duplication

--- modules/age.nix
+++ modules/age.nix
@@ -40,7 +40,7 @@ let
     _agenix_generation="$(basename "$(readlink ${cfg.secretsDir})" || echo 0)"
   '';
   newGeneration = ''
-    _agenix_generation="$(basename "$(readlink ${cfg.secretsDir})" || echo 0)"
+    ${currentGeneration}
     (( ++_agenix_generation ))
     echo "[agenix] creating new generation in ${cfg.secretsMountPoint}/$_agenix_generation"
     mkdir -p "${cfg.secretsMountPoint}"
@@ -108,7 +108,7 @@ let
   '') cfg.identityPaths;
 
   cleanupAndLink = ''
-    _agenix_generation="$(basename "$(readlink ${cfg.secretsDir})" || echo 0)"
+    ${currentGeneration}
     (( ++_agenix_generation ))
     echo "[agenix] symlinking new secrets to ${cfg.secretsDir} (generation $_agenix_generation)..."
     ln -sfT "${cfg.secretsMountPoint}/$_agenix_generation" ${cfg.secretsDir}
